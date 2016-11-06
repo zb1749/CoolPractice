e304f6543a3b98c5b24e2dc18ac84e573b7fb1d6
rain7night <rainnight@outlook.com>
2016-11-06 23:53:45 +0800

    list sort

---
+++ src/collection/sort/ListSort.java
@@ -0,0 +1,19 @@
+package collection.sort;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by kevin on 2016/11/6.
+ */
+public class ListSort {
+    public static void main(String[] args) {
+        List l = new ArrayList();
+        l.add(2);
+        l.add(1);
+        l.add(8);
+        System.out.println(l.toString());
+        //l.sort((o1,o2)->(Integer.valueOf(o1)-Integer.valueOf(o2)));
+
+    }
+}
